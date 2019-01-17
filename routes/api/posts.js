2e0dc4c053b41814d35d3555a56f244173a62103
Richard Horrocks <richardjhorrocks@gmail.com>
2019-01-17 12:36:19 +0000

    Addition of front-end code.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -144,4 +144,67 @@ router.post(
   }
 );
 
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post.
+// @access  Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation.
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id).then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      };
+
+      // Add to comments array.
+      post.comments.unshift(newComment);
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(() => res.status(404).json({ postnotfound: 'No post found' }));
+    });
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment.
+// @access  Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id).then(post => {
+      // Check if the comment exists.
+      if (
+        post.comments.filter(
+          comment => comment._id.toString() === req.params.comment_id
+        ).length === 0
+      ) {
+        return res
+          .status(404)
+          .json({ commentnotfound: 'Comment does not exist' });
+      }
+
+      // Get the remove index.
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+      // Splice out of the array.
+      post.comments.splice(removeIndex, 1);
+      post.save().then(post => res.json(post));
+    });
+  }
+);
+
 module.exports = router;
